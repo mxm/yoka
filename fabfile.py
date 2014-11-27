d346395320dad52faaf09e1cd8ee85bdfbbe1e7a
Max <max@posteo.de>
2014-11-27 19:03:37 +0100

    run benchmarks grouped in suites from benchmarks folder
    each module is a suite whose benchmarks are run by executing the suite object
    stored in the SUITE variable

--- fabfile.py
+++ fabfile.py
@@ -1,45 +1,87 @@
 from __future__ import with_statement
-from fabric.api import env, local, settings, abort, run, cd, execute
-from fabric.contrib.console import confirm
-from fabric.decorators import task, roles, runs_once, parallel
+from fabric.api import execute
 
-from utils import LocalCommand, RemoteCommand
+from cluster import gcloud, maintenance, hadoop, flink
 
-import gcloud
-import maintenance
-import hadoop
-import flink
-#import peel
+import pkgutil
 
-execution = [
-    gcloud.create_instances,
-    maintenance.update_package_cache,
-    maintenance.install_dependencies,
-    maintenance.set_java_home,
-    maintenance.set_key,
-    hadoop.install,
-    hadoop.configure,
-    hadoop.pull,
-    hadoop.master,
-    hadoop.slaves,
-    flink.install,
-    flink.configure,
-    flink.pull,
-    flink.master,
-    flink.slaves,
-]
+import benchmarks
 
-def setup_flink():
-    for step in execution:
-        print "executing", step.__name__
-        execute(step)
+class Experiment(object):
 
-def all():
-    paas_authenticate()
-    paas_create_instances()
-    cluster_install_dependencies()
-    create_peel_config()
-    deploy_peel()
-    start_peel()
-    get_results()
-    paas_delete_instances()
+    def setup(self):
+        raise NotImplementedError()
+
+    def run(self, job):
+        raise NotImplementedError()
+
+    def shutdown(self):
+        raise NotImplementedError()
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class Suite(Experiment):
+
+    def __init__(self, experiments):
+        self.experiments = experiments
+
+    def run(self):
+        for experiment in self.experiments:
+            print "running", experiment
+            experiment.setup()
+            experiment.run()
+            experiment.shutdown()
+
+
+class FlinkBenchmark(Experiment):
+
+    def setup(self):
+         execute(flink.master)
+         execute(flink.slaves)
+
+    def run(self):
+        raise NotImplementedError()
+
+    def shutdown(self):
+        execute(flink.slaves, "stop")
+        execute(flink.master, "stop")
+
+class FlinkBenchmarkSuite(Suite):
+
+    def __init__(self, *args, **kwargs):
+        super(FlinkBenchmarkSuite, self).__init__(*args, **kwargs)
+
+    def setup(self):
+        [execute(step) for step in [
+            gcloud.create_instances,
+            maintenance.update_package_cache,
+            maintenance.install_dependencies,
+            maintenance.set_java_home,
+            maintenance.set_key,
+            hadoop.install,
+            hadoop.configure,
+            hadoop.pull,
+            hadoop.master,
+            hadoop.slaves,
+            flink.install,
+            flink.configure,
+            flink.pull]
+        ]
+
+    def shutdown(self):
+        execute(hadoop.slaves, "stop")
+        execute(hadoop.master, "stop")
+        execute(gcloud.delete_instances)
+
+
+if __name__ == '__main__':
+    for importer, modname, ispkg in pkgutil.iter_modules(benchmarks.__path__):
+        if not ispkg:
+            print "running %s" % modname
+            module = __import__("benchmarks.%s" % modname, globals(), locals(), ["SUITE"], -1)
+            suite = module.SUITE
+            suite.setup()
+            suite.run()
+            suite.shutdown()
