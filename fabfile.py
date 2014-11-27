24b32df20c58bb72d267768a0c52989a12ab005f
Max <max@posteo.de>
2014-11-27 16:26:16 +0100

    add complete flink setup run

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-from fabric.api import env, local, settings, abort, run, cd
+from fabric.api import env, local, settings, abort, run, cd, execute
 from fabric.contrib.console import confirm
 from fabric.decorators import task, roles, runs_once, parallel
 
@@ -10,6 +10,29 @@ import maintenance
 import hadoop
 import flink
 #import peel
+
+execution = [
+    gcloud.create_instances,
+    maintenance.update_package_cache,
+    maintenance.install_dependencies,
+    maintenance.set_java_home,
+    maintenance.set_key,
+    hadoop.install,
+    hadoop.configure,
+    hadoop.pull,
+    hadoop.master,
+    hadoop.slaves,
+    flink.install,
+    flink.configure,
+    flink.pull,
+    flink.master,
+    flink.slaves,
+]
+
+def setup_flink():
+    for step in execution:
+        print "executing", step.__name__
+        execute(step)
 
 def all():
     paas_authenticate()
