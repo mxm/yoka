a7b31a6009d699e0eaf4d5fa7fadc272f7b77bc8
Max <max@posteo.de>
2014-12-02 15:44:47 +0100

    let experiments to be benchmarked reside under /experiments

--- experiments/wordcount.py
+++ experiments/wordcount.py
@@ -1,4 +1,4 @@
-from fabfile import FlinkBenchmarkSuite, FlinkBenchmark
+from lib import Experiment
 
 from cluster.maintenance import install
 from cluster.utils import master, render_template, exec_bash
@@ -6,25 +6,20 @@ from cluster.flink import run_jar
 from cluster.hadoop import copy_to_hdfs
 
 from fabric.api import env
-#ARGS="$HDFS_WC $HDFS_WC_OUT"
-#echo "running wc with args $ARGS"
-#$FLINK_BUILD_HOME"/bin/flink" run -p $DOP -j $FLINK_BUILD_HOME/examples/flink-java-examples-*-WordCount.jar $ARGS
 
+class WordCount(Experiment):
 
-class Run(FlinkBenchmark):
-
-    def __init__(self, parameters):
-        self.params = parameters
+    def __init__(self, params):
+        self.params = params
 
     def setup(self):
-        super(Run, self).setup()
         # generate wc data
         master(lambda: install("wget"))
         master(lambda: install("ruby"))
         master(lambda: install("bzip2"))
         master(lambda: install("aspell"))
         generate_wc_data = render_template(
-            "benchmarks/gen_wc_data.sh.mustache",
+            "experiments/wordcount_files/gen_wc_data.sh.mustache",
             self.params
         )
         master(lambda: exec_bash(generate_wc_data))
@@ -32,35 +27,16 @@ class Run(FlinkBenchmark):
                                     "generated-wc%d.txt" % self.params['id']))
 
 
-
     def run(self):
-        fun = lambda: run_jar("benchmarks",
+        master (lambda: run_jar("experiments/wordcount_files/",
                               "flink-java-examples-0.8-incubating-SNAPSHOT-WordCount.jar",
                               ["hdfs://%s:50040/generated-wc%d.txt"
                                % (env.master, self.params['id']),
                                "hdfs://%s:50040/tmp/wc-out%d/"
                                % (env.master, self.params['id'])
                               ]
-                      )
-        master(fun)
+                        )
+        )
 
     def shutdown(self):
-        super(Run, self).shutdown()
-
-
-
-experiments = [
-
-    Run({
-        'id' : 1,
-        'num_lines' : 1000,
-    }),
-
-    Run({
-        'id' : 2,
-        'num_lines' : 1000000,
-    }),
-
-]
-
-SUITE = FlinkBenchmarkSuite(experiments)
+        master("rm -rf /tmp/wc-data/generated-wc.txt")
