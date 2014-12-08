5b0e5247e9f9f709efe8998330c1761238404904
Max <max@posteo.de>
2014-12-08 10:38:39 +0100

    add basic logging support

---
+++ log.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+root = logging.getLogger('pybench')
+root.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('logs/run.log')
+fh.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+root.addHandler(ch)
+root.addHandler(fh)
+
+def get_logger(*args):
+    return root
