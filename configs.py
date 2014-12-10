8ad9f8514df6709bdd24fac5c0f0febd7d5ae7be
Max <max@posteo.de>
2014-12-10 16:08:16 +0100

    use tez master

--- configs.py
+++ configs.py
@@ -54,7 +54,7 @@ Standard Tez config
 """
 tez_config = {
         'git_repository' : 'https://github.com/apache/tez.git',
-        'git_commit' : '043c2c1511f1134add20e98e69a0fab49702667f', # 0.5.3 rc1
+        'git_commit' : 'master',
         'path' : "/home/%s/tez" % USER,
         'path_client' : "/home/%s/tez_client/" % USER,
     }
