8efc683736b8d5aec21baa86828c5c6b798b562b
Max <max@posteo.de>
2014-12-11 19:14:24 +0100

    update fabfile and include init method to resume cluster debugging

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,49 @@
-from __future__ import with_statement
-from fabric.decorators import task
-from fabric.api import execute, env
-
-from cluster.utils import get_slave_id
+#
+# This file defines the available targets for the fabric command "fab"
+#
+# For a list of all cluster commands, execute
+#
+# $ fab -l
+#
+# Available commands:
+#
+#     flink.configure
+#     flink.copy_log_master
+#     flink.copy_log_slaves
+#     flink.get_flink_dist_path
+#     flink.install
+#     flink.master
+#     flink.pull
+#     flink.pull_from_master
+#     flink.run_jar
+#     flink.slaves
+#     gcloud.authenticate
+#     gcloud.configure_ssh
+#     gcloud.create_config
+#     gcloud.create_instances
+#     gcloud.delete_instances
+#     gcloud.install_gcloud
+#     hadoop.configure
+#     hadoop.delete_data_slaves
+#     hadoop.install
+#     hadoop.master
+#     hadoop.pull
+#     hadoop.pull_from_master
+#     hadoop.run_jar
+#     hadoop.set_java_home
+#     hadoop.slaves
+#     maintenance.generate_key
+#     maintenance.install
+#     maintenance.install_dependencies
+#     maintenance.pull_from_master
+#     maintenance.set_java_home
+#     maintenance.set_key
+#     maintenance.update_package_cache
+#     maintenance.upgrade
+#     tez.configure
+#     tez.get_tez_tarball_path
+#     tez.install
 
 from cluster import gcloud, maintenance, hadoop, flink, tez, utils
+
+gcloud.init()
