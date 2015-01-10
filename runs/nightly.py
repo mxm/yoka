8ca4164ae643efb47aa329e468f9f7f4da020505
Max <max@posteo.de>
2015-01-10 22:23:42 +0100

    change nightly suite configuration

--- runs/nightly.py
+++ runs/nightly.py
@@ -17,14 +17,22 @@ from experiments import generators
 # Aljoscha's project
 compute_engine_config['project_name'] = "astral-sorter-757"
 # machine prefix
-compute_engine_config['prefix'] = "max-benchmark-"
+compute_engine_config['prefix'] = "nightly-benchmark-"
 # 2 cores 7.5GB RAM
 compute_engine_config['machine_type'] = "n1-standard-2"
 # num cores to use
 compute_engine_config['num_cores'] = 2
+compute_engine_config['size_mem'] = 7500
 # 16 workers + 1 master
-compute_engine_config['num_workers'] = 16
+compute_engine_config['num_workers'] = 10
 compute_engine_config['disk_space_gb'] = 200
+
+dop = compute_engine_config['num_workers'] * compute_engine_config['num_cores']
+
+flink_config['num_task_slots'] = compute_engine_config['num_cores']
+flink_config['taskmanager_heap'] = 5120 #5gb
+flink_config['jobmanager_heap'] = 5120
+flink_config['parallelization'] = dop
 
 cluster = ComputeEngine(compute_engine_config)
 hadoop = Hadoop(hadoop_config)
@@ -33,6 +41,14 @@ flink = Flink(flink_config)
 systems = [hadoop, flink]
 
 benchmarks = [
+
+    Benchmark(
+        id = "WordCount",
+        systems = [flink],
+        experiment = WordCountNew(),
+        times = 3
+    ),
+
     Benchmark(
         id = "Grep",
         systems = [flink],
@@ -40,12 +56,10 @@ benchmarks = [
         times = 3
     ),
 
-    Benchmark(
-        id = "WordCount-new",
-        systems = [flink],
-        experiment = WordCountNew(),
-        times = 3
-    ),
+    # KMeans
+    # PageRank
+    # ALS
+
 ]
 
 
@@ -54,14 +68,14 @@ generators = [
         id = "TextGenerator",
         systems = [flink],
         experiment = generators.Text(
-            size_gb = 512, # 512 gB of data
-            dop = compute_engine_config['num_workers'] * compute_engine_config['num_cores']
+            size_gb = 150, # 5gb * 10 nodes * 3
+            dop = dop
         )
     )
 ]
 
-suite = ClusterSuite("DefaultSuite", cluster, systems, generators, benchmarks)
+suite = ClusterSuite("NightlySuite", cluster, systems, generators, benchmarks)
 
 suite.execute(retry_setup=0,
-              shutdown_on_failure=False,
+              shutdown_on_failure=True,
               email_results=True)
