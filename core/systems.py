930e7202de591344e341c9d4f94b21f21b6463f4
Max <max@posteo.de>
2015-06-09 16:26:29 +0200

    Flink: remove special YARN log copying

--- core/systems.py
+++ core/systems.py
@@ -138,12 +138,6 @@ class FlinkYarn(Flink):
     def stop(self):
         execute(flink.master, 'stop', yarn=True)
 
-    def save_log(self, unique_full_path):
-        pass
-        # TODO
-        #execute(flink.copy_log_master, unique_full_path, yarn=True)
-        #execute(flink.copy_log_slaves, unique_full_path, yarn=True)
-
     def __str__(self):
         return "flink-yarn"
 
